f544652d46e94043ba83f66cd8ff751b99465686
Yubin Oh <sale506559@gmail.com>
2024-11-11 13:16:06 +0900

    fix: k1-1

--- K1.js
+++ K1.js
@@ -1,10 +1,6 @@
 function showCategory(categoryId) {
     const categories = document.querySelectorAll('.product-list');
     categories.forEach(category => {
-        if (category.id === categoryId) {
-            category.style.display = 'flex';  // 선택한 카테고리는 표시
-        } else {
-            category.style.display = 'none';  // 다른 카테고리는 숨김
-        }
+        category.style.display = (category.id === categoryId) ? 'flex' : 'none';
     });
 }
